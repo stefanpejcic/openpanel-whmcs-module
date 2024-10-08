57d5cfa9ef82664175160eb8030639e79764574b
Stefan Pejcic <stefan@pejcic.rs>
2024-10-09 00:59:31 +0200

    Update openpanel.php

--- openpanel.php
+++ openpanel.php
@@ -7,7 +7,7 @@
 # Created: 01.05.2024
 # Last Modified: 08.10.2024
 # Company: openpanel.com
-# Copyright (c) openpanel.com
+# Copyright (c) Stefan Pejcic
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
